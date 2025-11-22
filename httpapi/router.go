7025d7ab7f0566ab8642742d3353bd41a2e70933
NullSpook <nullspook@ensporia.com>
2025-11-22 11:35:22 +0700

    Use snake_case for paths

--- httpapi/router.go
+++ httpapi/router.go
@@ -48,14 +48,14 @@ func NewRouter(rngController *RngController, rngHealthChecker healthz.HealthChec
 	router.MuxRouter.HandleFunc("/api/randintegers", rngController.RandIntegers).Methods("GET")
 	router.MuxRouter.HandleFunc("/api/randuniform", rngController.RandUniform).Methods("GET")
 	router.MuxRouter.HandleFunc("/api/randnormal", rngController.RandNormal).Methods("GET")
-	router.MuxRouter.HandleFunc("/api/randbooleans-biasamplified", rngController.RandBooleansBiasAmplified).Methods("GET")
+	router.MuxRouter.HandleFunc("/api/randbooleans_biasamplified", rngController.RandBooleansBiasAmplified).Methods("GET")
 
 	router.MuxRouter.HandleFunc("/api/streambooleans", rngController.StreamBooleans).Methods("GET")
 	router.MuxRouter.HandleFunc("/api/streambytes", rngController.StreamBytes).Methods("GET")
 	router.MuxRouter.HandleFunc("/api/streamintegers", rngController.StreamIntegers).Methods("GET")
 	router.MuxRouter.HandleFunc("/api/streamuniform", rngController.StreamUniform).Methods("GET")
 	router.MuxRouter.HandleFunc("/api/streamnormal", rngController.StreamNormal).Methods("GET")
-	router.MuxRouter.HandleFunc("/api/streambooleans-biasamplified", rngController.StreamBooleansBiasAmplified).Methods("GET")
+	router.MuxRouter.HandleFunc("/api/streambooleans_biasamplified", rngController.StreamBooleansBiasAmplified).Methods("GET")
 
 	router.MuxRouter.Use(LoggingMiddleware)
 
